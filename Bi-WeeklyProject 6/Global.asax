22e73d483c3c927572e5e383f470c691834d93e6
George Chaniotakis <haniotinho@gmail.com>
2019-02-28 18:45:08 +0200

    Add project files.

---
+++ Bi-WeeklyProject 6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bi_WeeklyProject_6.MvcApplication" Language="C#" %>
---
+++ Bi-WeeklyProject 6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bi_WeeklyProject_6.Startup))]
+namespace Bi_WeeklyProject_6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
