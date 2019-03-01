c4555871da25dc7aebcbacae47ff5802a3333369
George Chaniotakis <haniotinho@gmail.com>
2019-03-01 18:50:43 +0200

    login controller

---
+++ Bi-WeeklyProject 6/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity;
+using Bi_WeeklyProject_6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bi_WeeklyProject_6.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(string Username, string Password)
+        {
+            UserManager manager = new UserManager();
+            var loggedInUser = manager.Login(Username, Password);
+
+            if (loggedInUser != null)
+            {
+                Session["user"] = loggedInUser;
+                return View("Success", loggedInUser);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
