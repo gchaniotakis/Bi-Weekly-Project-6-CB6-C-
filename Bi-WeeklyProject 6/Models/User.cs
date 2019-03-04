b69b4b10db3a5d3a2a1487d09aca9c5c4fd8b89a
George Chaniotakis <haniotinho@gmail.com>
2019-03-04 23:55:39 +0200

    User class

---
+++ Bi-WeeklyProject 6/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bi_WeeklyProject_6.Models;
+
+namespace Bi_WeeklyProject6.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username Required !")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password Required !")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Role Required !")]
+        public Roles Role { get; set; }
+
+
+        public IEnumerable<Task> Tasks { get; set; }
+    }
+
+    public enum Roles
+    {
+        Analyst,
+        Architect,
+        Programmer,
+        Tester,
+        Manager
+    }
+}
