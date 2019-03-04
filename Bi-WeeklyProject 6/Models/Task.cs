4c1ff247709531e02975f498234e3167f729ee18
George Chaniotakis <haniotinho@gmail.com>
2019-03-04 19:06:44 +0200

    Task class

---
+++ Bi-WeeklyProject 6/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bi_WeeklyProject_6.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Text Title")]
+        public string Name { get; set; }
+        [Required]
+        public string Text { get; set; }
+
+
+    }
+}
